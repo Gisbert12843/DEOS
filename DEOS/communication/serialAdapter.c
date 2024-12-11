5d3ed7f1d053e9955b9af3e17f1af7ffb171663f
Kai Oestreich <51728041+Gisbert12843@users.noreply.github.com>
2024-12-11 15:57:16 +0100

    prak3 first work

---
+++ DEOS/communication/serialAdapter.c
@@ -0,0 +1,172 @@
+/*!
+ *  \brief Layer built on top of UART where frames get assembled.
+ *
+ *  \author   Fachbereich 5 - FH Aachen
+ *  \date     2024
+ *  \version  1.0
+ */
+
+#include "serialAdapter.h"
+#include "../lib/lcd.h"
+#include "../lib/util.h"
+#include "../os_core.h"
+#include "../os_scheduler.h"
+#include "rfAdapter.h"
+#include "xbee.h"
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdbool.h>
+#include <string.h>
+
+//----------------------------------------------------------------------------
+// Defines
+//----------------------------------------------------------------------------
+
+//! Timeout for receiving frames
+#define SERIAL_ADAPTER_READ_TIMEOUT_MS ((time_t)500)
+
+//----------------------------------------------------------------------------
+// Forward declarations
+//----------------------------------------------------------------------------
+
+//! Calculates a checksum of given data
+void serialAdapter_calculateChecksum(checksum_t *checksum, void *data, uint8_t length);
+
+//! Calculates a checksum of the frame
+void serialAdapter_calculateFrameChecksum(checksum_t *checksum, frame_t *frame);
+
+//! Returns true if timestamp + timeoutMs is a timestamp in the past
+bool serialAdapter_hasTimeout(time_t timestamp, time_t timeoutMs);
+
+//----------------------------------------------------------------------------
+// Given functions
+//----------------------------------------------------------------------------
+
+/*!
+ *  Checks if a given timestamp has timed out
+ *
+ *  \param timestamp Timestamp to check
+ *  \param timeoutMs Timeout in milliseconds
+ *  \return True if timestamp + timeoutMs is a timestamp in the past
+ */
+bool serialAdapter_hasTimeout(time_t timestamp, time_t timeoutMs)
+{
+	return (getSystemTime_ms() - timestamp >= timeoutMs);
+}
+
+/*!
+ *  Blocks process until at least one byte is available to be read
+ */
+void serialAdapter_waitForAnyByte()
+{
+	while (xbee_getNumberOfBytesReceived() == 0)
+	{
+		os_yield();
+	}
+}
+
+//----------------------------------------------------------------------------
+// Your Homework
+//----------------------------------------------------------------------------
+
+/*!
+ *  Initializes the serialAdapter and their dependencies
+ */
+void serialAdapter_init(void)
+{
+	#warning [Praktikum 3] Implement here
+	xbee_init();
+}
+
+/*!
+ *  Sends a frame with given innerFrame
+ *
+ *  \param destAddr where to send the frame to
+ *  \param length how many bytes the innerFrame has
+ *  \param innerFrame buffer as payload of the frame
+ */
+void serialAdapter_writeFrame(address_t destAddr, inner_frame_length_t length, inner_frame_t *innerFrame)
+{
+	#warning [Praktikum 3] Implement here
+
+	// Prepare header
+
+	// Write header and calculate its checksum
+
+	// Write inner frame and calculate its checksum
+
+	// Prepare footer and write it
+
+}
+
+/*!
+ *  Blocks process until byteCount bytes are available to be read.
+ *
+ *  \param byteCount Count of bytes that need to arrive so that the function will unblock
+ *  \param frameTimestamp Start time of the first byte arrived from which the timeout will be calculated on
+ *  \return False when it times out.
+ */
+bool serialAdapter_waitForData(uint8_t byteCount, time_t frameTimestamp)
+{
+	#warning [Praktikum 3] Implement here
+	
+}
+
+/*!
+ *  Reads incoming data and processes it. Needs to be called periodically.
+ *  Don't read from UART in any other process while this is running.
+ */
+void serialAdapter_worker()
+{
+	#warning [Praktikum 3] Implement here
+
+	// Wait for data
+
+	// Wait for arrival of complete header
+
+	// Parse header one by one, abort if first byte is not part of the start flag
+
+	// Wait for complete inner frame and footer
+
+	// Read inner frame
+
+	// Read checksum
+
+	// Verify checksum
+
+	// Check if we are addressed by this frame
+
+	// Forward to next layer
+
+}
+
+/*!
+ *  Calculates a checksum of given data
+ *
+ *  \param checksum pointer to a checksum that will be updated
+ *  \param data buffer on which the checksum will be calculated
+ *  \param length size of the given buffer
+ */
+void serialAdapter_calculateChecksum(checksum_t *checksum, void *data, uint8_t length)
+{
+	#warning [Praktikum 3] Implement here
+	
+	for(uint8_t i = 0; i<length;i++ )
+	{
+		*checksum ^= ((uint8_t*)data)[i];
+	}
+}
+
+/*!
+ *  Calculates a checksum of the frame
+ *
+ *  \param checksum pointer to a checksum that will be updated
+ *  \param frame data on which the checksum will be calculated
+ */
+void serialAdapter_calculateFrameChecksum(checksum_t *checksum, frame_t *frame)
+{
+	serialAdapter_calculateChecksum(checksum,&frame->header,sizeof(frame_header_t));
+	serialAdapter_calculateChecksum(checksum,&frame->innerFrame,sizeof(inner_frame_t));
+}
---
+++ DEOS/communication/xbee.c
@@ -0,0 +1,145 @@
+/*!
+ *  \brief Layer forwarding the underlying UART library.
+ *
+ *  \author   Fachbereich 5 - FH Aachen
+ *  \date     2024
+ *  \version  1.0
+ */
+
+#include "xbee.h"
+#include "../lib/uart.h"
+#include "../os_scheduler.h"
+#include "rfAdapter.h"
+
+#include <avr/interrupt.h>
+
+//----------------------------------------------------------------------------
+// Your Homework
+//----------------------------------------------------------------------------
+
+/*!
+ *  Initializes the XBee
+ */
+void xbee_init()
+{
+	#warning [Praktikum 3] Implement here
+	uart1_init(UART_BAUD_SELECT(38400,16000000UL));
+	
+}
+
+/*!
+ *  Transmits one byte to the XBee
+ *
+ *  \param byte one byte that will be sent through UART
+ */
+void xbee_write(uint8_t byte)
+{
+	uart1_putc(byte);
+}
+
+/*!
+ *  Transmits the given data to the XBee
+ *
+ *  \param data buffer with will be sent through UART
+ *  \param length size of the buffer
+ */
+void xbee_writeData(void *data, uint8_t length)
+{
+	for(int i = 0; i < length;i++)
+	{
+		xbee_write(*(((uint8_t*)data)+i));
+	}
+}
+
+/*
+ *  Receives one byte from the XBee
+ *
+ *  \param byte Reference parameter where the read byte will be written to
+ *  \return Error code or XBEE_SUCCESS. When XBEE_BUFFER_INCONSISTENCY gets returned, `byte` still gets updated
+ */
+uint8_t xbee_read(uint8_t *byte)
+{
+	//we are assuming that "int" from the uart library is implemented as uint16_t, kinda ugly ngl
+	uint16_t temp = (uint16_t)uart1_getc();
+	
+	switch(((uint8_t)(temp))+1)
+	{
+		case 0:
+		{
+			*byte = (uint8_t)temp;
+			return XBEE_SUCCESS;
+		}
+		break;
+		
+		case UART_OVERRUN_ERROR:
+		{
+			*byte = (uint8_t)temp;
+			return XBEE_BUFFER_INCONSISTENCY;
+		}
+		break;
+		
+		case UART_BUFFER_OVERFLOW:
+		{
+			*byte = (uint8_t)temp;
+			return XBEE_BUFFER_INCONSISTENCY;
+		}
+		break;
+		
+		case UART_FRAME_ERROR:
+		{
+			return XBEE_READ_ERROR;
+		}
+		break;
+		
+		case UART_NO_DATA:
+		{
+			return XBEE_DATA_MISSING;
+		}
+		break;
+		default:
+			break;
+	}
+	
+	return 255;
+}
+
+
+
+/*!
+ *	Returns current filling of the buffer in byte
+ *
+ *  \return count of bytes that can be received through `xbee_read`
+ */
+uint16_t xbee_getNumberOfBytesReceived()
+{
+	return uart1_getrxcount();
+}
+
+/*!
+ *  Receives `length` bytes and writes them to `buffer`. Make sure there are enough bytes to be read
+ *
+ *	\param message_buffer Buffer where to store received bytes
+ *  \param buffer_size Amount of bytes that need to be received
+ *  \return error Code if there is any. If it's not XBEE_SUCCESS, you shouldn't use the result
+ */
+uint8_t xbee_readBuffer(uint8_t *buffer, uint8_t length)
+{
+	if(xbee_getNumberOfBytesReceived() < length)
+		return XBEE_DATA_MISSING;
+	
+	uint8_t temp_buff[length];
+	for(uint8_t i = 0; i<length;i++)
+	{
+		uint8_t err = xbee_read(&temp_buff[i]);
+		if(err != XBEE_SUCCESS)
+			return err;
+	}
+	*buffer = temp_buff;
+	
+	return XBEE_SUCCESS;
+
+	// Early return if not enough bytes are available
+
+	// Copy received data into destination buffer, check for errors, early return if an error occurs
+
+}
